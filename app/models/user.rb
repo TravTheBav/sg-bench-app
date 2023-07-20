f9b4c0f55b4939b6ebdba8126227c3d80471675e
TravTheBav <tzito93@gmail.com>
2023-07-20 17:13:10 -0400

    Remove user avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,26 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable  
-  validate :acceptable_avatar
 
   has_many :build_orders, dependent: :destroy
   has_many :attack_benchmarks, dependent: :destroy
-  has_one_attached :avatar
 
   def admin?
     admin
-  end
-
-  def acceptable_avatar
-    return unless avatar.attached?
-
-    unless avatar.blob.byte_size <= 1.megabyte
-      errors.add(:avatar, "is too big")
-    end
-
-    acceptable_types = ["image/jpeg", "image/png"]
-    unless acceptable_types.include?(avatar.content_type)
-      errors.add(:avatar, "must be a JPEG or PNG")
-    end
   end
 end
